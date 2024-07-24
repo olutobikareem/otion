d938e6b0f1c4a5442a46a3ba57635b33ba684b07
Olu Kareem <olukareem@gmail.com>
2024-07-23 19:39:31 -0700

    ui: major update to sidebar. added search, settings, new page options

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-import { Navigation } from "./(components)/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();

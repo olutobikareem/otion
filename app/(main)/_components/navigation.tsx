45257bf21547625a6f42e66b7c349f6b99252b34
Olu Kareem <olukareem@gmail.com>
2024-07-24 16:56:19 -0700

    Feat: added trash feature

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
+import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
 import {
   ChevronLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
-import { useMediaQuery } from "usehooks-ts";
-import { useMutation } from "convex/react";
-import { UserItem } from "./user-items";
-import { Item } from "./item";
 import { toast } from "sonner";
+import { useMediaQuery } from "usehooks-ts";
 import { DocumentList } from "./document-list";
+import { Item } from "./item";
+import { TrashBox } from "./trash-box";
+import { UserItem } from "./user-items";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -141,6 +149,17 @@ export const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? "bottom" : "right"}
+            >
+<TrashBox/>            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
